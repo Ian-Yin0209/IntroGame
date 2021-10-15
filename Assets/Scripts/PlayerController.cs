c5eef2bf407526e0bf2478ce42ba856b9f0e0fc0
Qi Zhu <zq11110320@gmail.com>
2021-10-15 10:07:17 +0100

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue;
     public float speed;
+    private int count;
+    private readonly int numPickups = 8;
+    public Text scoreText;
+    public Text winText;
+
+    private void Start()
+    {
+        count = 0;
+        winText.text = "";
+        SetCountText(); 
+    }
 
     void OnMove(InputValue value)
     {
@@ -17,4 +29,22 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
         GetComponent<Rigidbody>().AddForce(speed * Time.fixedDeltaTime * movement);
     }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+        }
+        count++;
+        SetCountText();
+    }
+    private void SetCountText()
+    {
+        scoreText.text = "Score: " + count.ToString();
+        if (count >= numPickups)
+        {
+            winText.text = "You Win!";
+        }
+    }
 }
+
