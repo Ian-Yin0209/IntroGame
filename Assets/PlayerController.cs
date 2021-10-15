b3fc447f1a8db2754c811c0cb4bd085d07e693c2
Ian-Yin0209 <69559364+Ian-Yin0209@users.noreply.github.com>
2021-10-15 16:48:39 +0800

    part2.2

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 moveValue;
+    public float speed;
+    private int variable=0;
+    void OnMove(InputValue value)
+    {
+        moveValue = value.Get<Vector2>();
+    }
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
+        GetComponent<Rigidbody>().AddForce(speed * Time.fixedDeltaTime * movement);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+            }
+        variable = variable + 1;
+        }
+}
